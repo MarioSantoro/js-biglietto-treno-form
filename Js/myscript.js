6bf69aed3cf491119a2fbc3f7ec8b4c5293c64ef
MarioSantoro <mariosantoro124@gmail.com>
2023-05-15 17:06:16 +0200

    Milestone 2 complete

--- Js/myscript.js
+++ Js/myscript.js
@@ -2,17 +2,25 @@ let element = document.getElementById("sumbit");
 let TicketCost;
 element.addEventListener("click" , 
     function(){
-        const AgeTraverel = parseInt(document.getElementById("Age").value);
-        const Km = parseInt(document.getElementById("Km").value);
+        const AgeTraverel = parseInt(document.getElementById("floatingAge").value);
+        const Km = parseInt(document.getElementById("floatingKm").value);
         TicketCost = Km * 0.233;
         if(AgeTraverel < 18){
             //applica sconto del 19.4% se è minorenne
-            TicketCost= TicketCost - ((19.4 / 100) * TicketCost)
+            TicketCost= TicketCost - ((19.4 / 100) * TicketCost);
+            document.getElementById("Sale").innerHTML = "Lo sconto applicato è di 19.4%"
         } else if(AgeTraverel >= 65){
             //applica sconto del 37.7% se è anziano
-            TicketCost= TicketCost - ((37.7 / 100) * TicketCost)
+            TicketCost= TicketCost - ((37.7 / 100) * TicketCost);
+            document.getElementById("Sale").innerHTML = "Lo sconto applicato è di 37.7%"
+        } else{
+            document.getElementById("Sale").innerHTML = "Non hai diritto a nessun tipo di sconto"
         }
-        console.log(TicketCost.toFixed(2))
+
+        document.getElementById("Age-ticket").innerHTML = AgeTraverel;
+        document.getElementById("km-ticket").innerHTML = Km + "Km";
+        document.getElementById("Price").innerHTML = TicketCost.toFixed(2) + "€";
+        console.log(TicketCost.toFixed(2));
     }
 )
 
